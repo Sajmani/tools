2891ea225e4db9472678f3332342fa12a03a5880
Sameer Ajmani <sameer@sameer-macbookpro.roam.corp.google.com>
2017-07-15 22:49:54 -0400

    testhelper: sketch idea for a new tool, Usage message

---
+++ testhelper/main.go
@@ -0,0 +1,39 @@
+// The testhelper command converts test helper functions to use t.Helper().
+package main
+
+// Usage describes the testhelper command line.
+var Usage = `Converts test helper functions to use t.Helper().
+
+  testhelper <function> <args>
+
+testhelper converts a function of the form
+  func F(a, b) (x, y, error)
+to one of the form:
+  func F(t *testing.T, a, b) (x, y)
+
+testhelper adds a call to t.Helper() to the beginning of F's body,
+and replaces return statements with non-nil errors with calls to t.Fatal:
+  return nil, 0, err
+to:
+  t.Fatal(err)
+
+testhelper converts calls to F of the form:
+  x, y, err := F(a, b)
+  if err != nil {
+    t.Fatal(err)
+  }
+to:
+  x, y := F(t, a, b)
+
+If F only returned an error, then testhelper
+also converts call sites of the form:
+  if err := F(a, b); err != nil {
+    t.Fatal(err)
+  }
+to:
+  F(t, a, b)
+`
+
+func main() {
+	
+}
